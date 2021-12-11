a81eac63a1965ce3c210506b26ed18dc87b119fa
jakobrs <jakobrs100@gmail.com>
2021-12-11 11:49:32 +0100

    Some changes

--- calculation/src/main.rs
+++ calculation/src/main.rs
@@ -3,20 +3,16 @@ use primesieve::PrimesieveIterator;
 fn main() {
     println!("Hello, world!");
 
-    let mut max_q = i64::MIN;
-
-    for (i, prime) in PrimesieveIterator::new_start_stop(0, 223092870)
+    let max_q = PrimesieveIterator::new_start_stop(0, 223092870)
         .take_while(|&p| p <= 223092870)
         .enumerate()
-    {
-        let i = i as i64;
-        let prime = prime as i64;
+        .map(|(i, prime)| {
+            let i = i as i64;
+            let prime = prime as i64;
 
-        let q = 6 * i - prime + 3;
-        if q > max_q {
-            max_q = q;
-        }
-    }
+            6 * i - prime + 3
+        })
+        .max();
 
-    println!("Max Q: {}", max_q);
+    println!("Max Q: {:?}", max_q);
 }
