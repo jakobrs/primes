f6484dd3cc8e86834521b4965bca2563023b6a8c
jakobrs <jakobrs100@gmail.com>
2021-12-26 11:48:42 +0100

    hgreiofdv

--- calculation/src/bin/other.rs
+++ calculation/src/bin/other.rs
@@ -1,8 +1,25 @@
+use std::{sync::{Arc, atomic::{AtomicUsize, Ordering}}, time::Instant};
+
 use itertools::Itertools;
 use rayon::prelude::*;
 
 fn main() {
     const M: usize = 2 * 3 * 5 * 7 * 11 * 13 * 17 * 19 * 23;
+
+    let count_iterations = Arc::new(AtomicUsize::new(0));
+
+    {
+        let count_iterations = count_iterations.clone();
+
+        std::thread::spawn(move || loop {
+            let before = Instant::now();
+            std::thread::sleep(std::time::Duration::from_secs(10));
+            println!(
+                "{}",
+                count_iterations.swap(0, Ordering::Relaxed) as f32 / before.elapsed().as_secs_f32()
+            );
+        });
+    }
 
     let iter = (1..3)
         .cartesian_product(1..5)
@@ -14,6 +31,9 @@ fn main() {
         .cartesian_product(1..23)
         .par_bridge()
         .map(|(((((((r3, r5), r7), r11), r13), r17), r19), r23)| {
+            count_iterations.fetch_add(1, Ordering::Relaxed);
+            eprintln!("a");
+
             let mut count = 1;
 
             (2..M + 1)
